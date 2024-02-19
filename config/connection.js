0c628b538f3ae2f4d6cdbff8a0d19d675b344778
Cain LeDuc <cain6gates@gmail.com>
2024-02-19 02:53:51 -0500

    postgres

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,24 @@
 const { Sequelize } = require('sequelize');
+const config = require('./config.js');
 
-const sequelize = new Sequelize('codecalm', 'cain', 'C41nF0x6283*', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+let sequelize;
+
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: config.dialect
+  });
+}
 
 module.exports = sequelize;
