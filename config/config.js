dc63b98b9a0d5455bebb5026d24323582ce542d1
Cain LeDuc <cain6gates@gmail.com>
2024-02-19 02:37:00 -0500

    postgres

--- config/config.js
+++ config/config.js
@@ -6,14 +6,14 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_DEVELOPMENT,
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql", // Or 'postgres', 'sqlite', 'mariadb', 'mssql' depending on your DB
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_TEST,
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql", // Or another dialect as per your setup
   },
   production: {
     use_env_variable: 'DATABASE_URL',
@@ -22,7 +22,7 @@ module.exports = {
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false
+        rejectUnauthorized: false // This is important to avoid SSL errors
       }
     }
   }
